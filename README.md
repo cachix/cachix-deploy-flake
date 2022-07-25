833db12d7dfed09f3af13d8d895f4077d9bd3e2e
Domen Kožar <domen@dev.si>
2022-07-24 21:55:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Functions to help manage Cachix Deploy when using flakes.
+
+See https://docs.cachix.org/deploy/deploying-to-agents for more.
---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+
+ inputs = {
+   darwin.url = "github:LnL7/nix-darwin";
+ };
+
+ outputs = { self, darwin }: {
+   lib = pkgs: {
+     # TODO: validate opts
+     json = opts: pkgs.writeText "cachix-deploy.json" (builtins.toJSON opts);
+     nixos = module: (pkgs.nixos module).toplevel;
+     darwin = module: (darwin.lib.darwinSystem {
+       system = pkgs.system;
+       modules = [
+         (darwin + "/pkgs/darwin-installer/installer.nix") 
+         module
+       ]
+     }).system;
+   };
+ };
+
+}
