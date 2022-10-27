3bbd456c199e026d9cf0ac1417fdbc18771fb901
Robert Helgesson <robert@rycee.net>
2022-10-27 23:55:45 +0200

    Add support for Home Manager

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
 
  inputs = {
    darwin.url = "github:LnL7/nix-darwin";
+   home-manager.url = "github:nix-community/home-manager";
  };
 
- outputs = { self, darwin }: {
+ outputs = { self, darwin, home-manager }: {
    lib = pkgs: {
      # TODO: validate opts
      spec = opts: pkgs.writeText "cachix-deploy.json" (builtins.toJSON opts);
@@ -16,6 +17,13 @@
          module
        ];
      }).system;
+
+     homeManager = { extraSpecialArgs ? { } }: module: let
+       result = home-manager.lib.homeManagerConfiguration {
+         inherit pkgs extraSpecialArgs;
+         modules = [ module ];
+       };
+     in result.config.home.activationPackage;
    };
  };
 
