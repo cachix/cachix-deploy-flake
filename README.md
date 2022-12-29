b5086071c34900fe5ab17090bf9a9b4c320de287
Domen Kožar <domen@dev.si>
2022-12-29 11:12:59 +0000

    add bootstrap NixOS/Hetzner support

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ See https://docs.cachix.org/deploy/deploying-to-agents for more.
 - `lib.spec`: takes attribute set of all options described in https://docs.cachix.org/deploy/reference
 - `lib.nixos` takes a module and returns nixos derivation
 - `lib.darwin` takes a module and returns nix-darwin derivation
+- `lib.homeManager` takes attribute set of options and returns home-manager
+- `lib.bootstrapNixOS` documented in https://github.com/cachix/cachix-deploy-hetzner-dedicacted
--- flake.nix
+++ flake.nix
@@ -3,10 +3,48 @@
  inputs = {
    darwin.url = "github:LnL7/nix-darwin";
    home-manager.url = "github:nix-community/home-manager";
+   home-manager.inputs.nixpkgs.follows = "nixpkgs";
+   nixos-remote.url = "github:numtide/nixos-remote";
+   nixos-remote.inputs.nixpkgs.follows = "nixpkgs";
+   nixos-remote.inputs.disko.follows = "disko";
+   disko.url = "github:nix-community/disko";
+   disko.inputs.nixpkgs.follows = "nixpkgs";
  };
 
- outputs = { self, darwin, home-manager }: {
-   lib = pkgs: {
+ outputs = { self, nixpkgs, darwin, home-manager, nixos-remote, disko, ... }: {
+
+   packages = let 
+       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+     in forAllSystems (system: let 
+         pkgs = nixpkgs.legacyPackages.${system};
+       in {
+        bootstrapHetzner = pkgs.writeScriptBin "bootstrap-hetzner" ''
+          #!${pkgs.runtimeShell}
+
+          # error out if not two arguments are given
+          if [ "$#" -ne 3 ]; then
+            echo "Usage: $0 <IP> <agent-hostname> <cachix-agent-token-path>"
+            echo "Example: $0 1.1.1.1 myagent ./mytoken.secret"
+            exit 1
+          fi
+
+          echo "Bootstrapping $2 on $1 ..."
+          echo "Make sure your ssh key is added to the ssh-agent to prevent multiple password prompts."
+
+          IP="$1"
+          agent="$2"
+          agenttokenpath="$3"
+
+          ${nixos-remote.packages.${system}.default}/bin/nixos-remote "root@$IP" --flake ".#$agent"
+
+          echo "Waiting for machine to reboot..."
+          until scp -o ConnectTimeout=10 $agenttokenpath root@$IP:/etc/cachix-agent.token; do sleep 5; done
+          ssh root@$IP systemctl restart cachix-agent
+
+          echo "Done."
+        '';
+     });
+    lib = pkgs: {
      # TODO: validate opts
      spec = opts: pkgs.writeText "cachix-deploy.json" (builtins.toJSON opts);
      nixos = module: (pkgs.nixos module).toplevel;
@@ -24,7 +62,38 @@
          modules = [ module ];
        };
      in result.config.home.activationPackage;
-   };
- };
 
+     bootstrapNixOS = { system, hostname, grubDevices ? [], diskoDevices ? import "${disko}/example/mdadm.nix" { disks = grubDevices; }, sshPubKey }: 
+      let 
+        module = { pkgs, ... }: {
+            imports = [
+              disko.nixosModules.disko
+            ];
+
+            config = {
+              services.cachix-agent.enable = true;
+              networking.hostName = hostname;
+              disko.devices = diskoDevices;
+              boot.loader.grub.devices = grubDevices;
+              # enable nvme https://github.com/nix-community/disko/issues/96
+              boot.initrd.availableKernelModules = [ "nvme" ];
+              
+              # add root ssh key
+              users.users.root.openssh.authorizedKeys.keys = [ sshPubKey ];
+              # enable ssh
+              services.openssh.enable = true;
+              # enable passwordless ssh for root 
+              services.openssh.permitRootLogin = "without-password";
+            };
+          };
+      in {
+        module = module;
+        nixos = nixpkgs.lib.nixosSystem {
+          system = system;
+          modules = [ module ];
+        };
+  };
+};
+ };
 }
+
