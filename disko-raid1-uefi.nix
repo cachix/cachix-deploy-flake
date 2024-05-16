ddc0c9116293203e407fe17e0c77ae2cc1e5a68c
Sander <hey@sandydoo.me>
2024-05-16 15:30:40 +0000

    disko: update disk config

--- disko-raid1-uefi.nix
+++ disko-raid1-uefi.nix
@@ -5,37 +5,29 @@
       type = "disk";
       device = disk;
       content = {
-        type = "table";
-        format = "gpt";
-        partitions = [
-          {
-            name = "boot";
-            start = "0";
-            end = "1M";
-            part-type = "primary";
-            flags = [ "bios_grub" ];
-          }
-          {
-            name = "ESP";
-            start = "1M";
-            end = "1GiB";
-            fs-type = "fat32";
-            bootable = true;
+        type = "gpt";
+        partitions = {
+          boot = {
+            size = "1M";
+            type = "EF02"; # for grub MBR
+            priority = 1;
+          };
+          ESP = {
+            size = "500M";
+            type = "EF00";
             content = {
               type = "mdraid";
               name = "boot";
             };
-          }
-          {
-            name = "nixos";
-            start = "1GiB";
-            end = "100%";
+          };
+          nixos = {
+            size = "100%";
             content = {
               type = "mdraid";
               name = "nixos";
             };
-          }
-        ];
+          };
+        };
       };
     });
   disko.devices.mdadm = {
@@ -53,9 +45,12 @@
       type = "mdadm";
       level = 1;
       content = {
-        type = "filesystem";
-        format = "ext4";
-        mountpoint = "/";
+        type = "gpt";
+        partitions.primary = {
+          type = "filesystem";
+          format = "ext4";
+          mountpoint = "/";
+        };
       };
     };
   };
